5b58eabfa4dd5100a91ac3cdd84ec0b3ddd9efa2
AhnDo0 <51705063+AhnDo0@users.noreply.github.com>
2021-07-27 19:51:38 +0900

    Create 1181.cpp

---
+++ 1_정렬/ADY/1181.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(string a, string b){
+	if(a.length() == b.length()){
+		for(int i=0; i<a.length(); i++){
+			if(a[i] != b[i])
+				return a[i] < b[i];
+		}
+	}
+	return a.length() < b.length();
+		
+}
+
+int main(){
+	int N;
+	string word;
+	vector<string> wlist;
+	
+	cin>>N;
+	for(int i=0; i<N; i++){
+		cin>>word;
+		wlist.push_back(word);
+	}
+	
+	sort(wlist.begin(), wlist.end(), compare);
+	
+	cout<<wlist[0]<<'\n';
+	
+	for(int j=1; j<=N; j++){
+		if(wlist[j-1] == wlist[j])
+			continue;
+		cout<<wlist[j]<<'\n';
+	}
+	
+	return 0;
+}
